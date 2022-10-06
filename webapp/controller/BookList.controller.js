f29a61ad94b34c84e56846ae956f25276f407195
kev <kbreto2911@gmail.com>
2022-10-06 19:02:55 -0400

    fix: book's filter

--- webapp/controller/BookList.controller.js
+++ webapp/controller/BookList.controller.js
@@ -7,34 +7,33 @@ sap.ui.define([
 ], function(Controller, JSONModel, formatter, Filter, FilterOperator){
 	"use strict";
 
-	return Controller.extend("root.controller.InvoiceList", {
+	return Controller.extend("root.controller.BookList", {
 		formatter: formatter,
 		onInit: function(){
-			let oViewModel = new JSONModel({
-				currency: "EUR"
-			});
-			this.getView().setModel(oViewModel, "view")
+			// let oViewModel = new JSONModel({
+			// 	currency: "EUR"
+			// });
+			// this.getView().setModel(oViewModel, "view")
 		},
-		onFilterInvoices: function(oEvent){
+		onFilterBooks: function(oEvent){
 			//build filter arr
 			let aFilter = [];
 			let sQuery = oEvent.getParameter("query");
-			console.log(sQuery);
+	
 			if(sQuery){
-				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+				aFilter.push(new Filter("title", FilterOperator.Contains, sQuery));
 			}
 
 			//FILTER BINDING
-			let oList = this.byId("invoiceList");
+			let oList = this.byId("bookList");
 			let oBinding = oList.getBinding("items");
-			console.log(oList);
 			oBinding.filter(aFilter);
 		},
 		onPress: function(oEvent) {
 			let oItem = oEvent.getSource();
 			let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("detail", {
-				invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+				invoicePath: window.encodeURIComponent(oItem.getBindingContext("books").getPath().substr(1))
 			})
 		}
 	})
