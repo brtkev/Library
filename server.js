b64fe50dbeb7e107c14410a3aa79aa225bbc793d
kev <kbreto2911@gmail.com>
2022-10-07 20:29:11 -0400

    feat: server with expressjs

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+//express config
+const app = express();
+app.use("/", express.static(path.join(__dirname, "./webapp")));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
