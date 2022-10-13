e97e8802cdd79aa778b645a632f9b9bba944046b
kev <kbreto2911@gmail.com>
2022-10-12 21:41:14 -0400

    feat: booklist message

--- webapp/controller/BookList.controller.js
+++ webapp/controller/BookList.controller.js
@@ -1,10 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-], function(Controller, JSONModel, formatter, Filter, FilterOperator){
+	"sap/m/MessageToast",
+], function(Controller, formatter, MessageToast){
 	"use strict";
 
 	return Controller.extend("root.controller.BookList", {
@@ -13,9 +11,10 @@ sap.ui.define([
 		
 		},
 		onFilterBooks: function(oEvent){
+			let model = this.getView().getModel();
 			let search = oEvent.getParameter("query");
 			if(!search){
-				this.getView().getModel().setData({books: []})
+				model.setData({...model.getData(), books: []})
 			}else{
 				//get attribute to filter
 				let attribute = this.byId("bookListSelect").getSelectedItem().getKey();
@@ -26,24 +25,59 @@ sap.ui.define([
 				fetch(url, { method: "GET" })
 				.then(r => r.json()
 				.then(data => {
-					let model = this.getView().getModel();
-					model.setData({
-						...model.getData(),
-					books : data
-					})
+					if(data){
+						model.setData({
+							...model.getData(),
+						books : data
+						})	
+					}else{
+						const googleUrl = "https://www.googleapis.com/books/v1/volumes?" + new URLSearchParams({
+							q: search
+						})
+						fetch(googleUrl, {method: "GET"})
+						.then(r => r.json()
+						.then(data => {
+							model.setData({
+								...model.getData(),
+								books: googleBooksFilter(data)
+							})
+							MessageToast.show("click an element to add it to the collection")
+						}))
+					}
 				}))
 
 			}
 		},
 		onPress: function(oEvent) {
-			console.log("click")
-			// let oItem = oEvent.getSource();
-			// let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			// oRouter.navTo("detail", {
-			// 	bookPath: window.encodeURIComponent(oItem.getBindingContext("books").getPath().substr(1))
+			// console.log("click")
+			let oItem = oEvent.getSource();
+			let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			// oRouter.navTo("create", {
+			// 	bookPath: window.encodeURIComponent(oItem.getBindingContext().getPath().substr(1))
 			// })
 		}
 	})
 })
 
 
+function googleBooksFilter(data){
+	let books = data.items.slice(0,50);
+	return books.map(book => {
+		let info = book.volumeInfo;
+		let thumbnail = null;
+		if(info.imageLinks && info.imageLinks.hasOwnProperty("thumbnail")){
+			thumbnail = info.imageLinks.thumbnail;
+		}
+		return {
+			title: info.title,
+			description: info.description,
+			authors: info.authors,
+			categories: info.categories,
+			img: thumbnail,
+			printdate: info.publishedDate,
+			editorial: info.publisher	,
+			source: "google ebooks"
+		}
+	})
+
+}
